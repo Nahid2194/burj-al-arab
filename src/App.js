a3721438ff19584de2456509dd366e6ee29ce5b3
Nahid2194 <nahidjc80@gmail.com>
2020-11-13 22:09:25 +0600

    Adding Private Route

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext();
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState();
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <h1>{loggedInUser.email}</h1>
+      <h1>{loggedInUser.name}</h1>
       <Router>
         <Header />
         <Switch>
@@ -25,9 +26,9 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/book/:bedType">
+          <PrivateRoute path="/book/:bedType">
             <Book />
-          </Route>
+          </PrivateRoute>
           <Route exact path="/">
             <Home />
           </Route>
