295fbf99aabc4a6905ce638cd3b2bfbde4aeb732
Nahid2194 <nahidjc80@gmail.com>
2020-11-13 21:32:35 +0600

    adding Google auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD71wj0ZBANOjAIuKXhL2_08aaVbDXKnNA",
+    authDomain: "burj-al-arab-nahid.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-nahid.firebaseio.com",
+    projectId: "burj-al-arab-nahid",
+    storageBucket: "burj-al-arab-nahid.appspot.com",
+    messagingSenderId: "594431100354",
+    appId: "1:594431100354:web:586f21f3362582ccefc25b"
+};
+export default firebaseConfig;
